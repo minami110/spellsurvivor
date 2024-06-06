f33fc5fa5889dcc9ea8f815eb78cb4e2755c6c4e
kadomatsu <tktkcap.com@gmail.com>
2024-06-06 17:51:55 +0900

    :art: reformat scripts

--- Infinity/Projectiles/RotateBook.cs
+++ Infinity/Projectiles/RotateBook.cs
@@ -1,4 +1,3 @@
-using System;
 using Godot;
 using R3;
 
@@ -6,73 +5,74 @@ namespace fms.Projectile;
 
 public partial class RotateBook : ProjectileBase
 {
-    [Export]
-    private RigidBody2D _rigidBody = null!;
+    private float _angularVelocity;
 
     [Export]
     private Area2D _enemyDamageArea = null!;
-    
+
+    [Export]
+    private RigidBody2D _rigidBody = null!;
+
+    private double _timer;
+
     public float Radius { get; set; }
 
     public float SecondPerRound { get; set; } = 1f;
 
-    public float Angle { get; set; } = 0f;
-    
+    public float Angle { get; set; }
+
     /// <summary>
     ///     複数の弾を同時に発射するときの初期位置のずれを設定するために利用する時間(second)
     ///     (クールダウン / 弾数)で導出する
     /// </summary>
-    
+
     // (中桐)名前終わっています 単位も統一できていないです
-    public float InitTimeForRelativePos { get; set; } = 0f;
-    
-    private float _angularVelocity = 0f;
-    
+    public float InitTimeForRelativePos { get; set; }
+
     public override void _Ready()
     {
         _rigidBody.Hide();
-        
+
         // Set rigidbody parameter
         _rigidBody.GlobalPosition = CalculatePosition(_timer, _angularVelocity, Radius);
         _rigidBody.RotationDegrees = Angle;
-        
+
         // Connect
         _enemyDamageArea.BodyEnteredAsObservable()
             .Cast<Node2D, Enemy>()
             .Subscribe(this, (x, state) => { state.OnEnemyBodyEntered(x); })
             .AddTo(this);
-        
+
         // Calculate Angular Velocity
         _angularVelocity = Mathf.DegToRad(360 / SecondPerRound);
 
         // Set relative position
         _timer = InitTimeForRelativePos;
-        
+
         _rigidBody.Show();
     }
 
-    private double _timer = 0;
     public override void _Process(double delta)
     {
         base._Process(delta);
-        
+
         _timer += delta;
 
         _rigidBody.GlobalPosition = CalculatePosition(_timer, _angularVelocity, Radius);
     }
-    
+
     private void OnEnemyBodyEntered(Enemy enemy)
     {
         enemy.TakeDamage(BaseDamage);
     }
-    
+
     private Vector2 CalculatePosition(double time, float angularVelocity, float radius)
     {
         var positionX = (float)Mathf.Cos(time * angularVelocity);
         var positionY = (float)Mathf.Sin(time * angularVelocity);
-        
+
         var unitVec = new Vector2(positionX, positionY);
-        
-        return (unitVec * radius) + Main.PlayerNode.GlobalPosition;
+
+        return unitVec * radius + Main.PlayerNode.GlobalPosition;
     }
 }
--- Infinity/Weapons/Book.cs
+++ Infinity/Weapons/Book.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using fms.Effect;
-using fms.Projectile;
+﻿using fms.Projectile;
 using Godot;
 
 namespace fms.Weapon;
@@ -17,10 +15,10 @@ public partial class Book : WeaponBase
     [Export]
     // Pixel
     private float _radius = 100f;
-    
+
     [Export]
     private Node2D _root = null!;
-    
+
     private protected override int BaseCoolDownFrame => 180;
 
     private protected override void DoAttack()
@@ -30,33 +28,32 @@ public partial class Book : WeaponBase
             // Level 1 は1つの弾をだす
             case 1:
             {
-                SpawnBullet(_radius, 1 , 0f);
+                SpawnBullet(_radius);
                 break;
             }
             // Level 2 は2つの弾をだす
             case 2:
             {
-                SpawnBullet(_radius, 2, 0f);
+                SpawnBullet(_radius, 2);
                 break;
             }
             // Level 3 以上は同じ
             default:
             {
-                SpawnBullet(_radius, 3, 0f);
+                SpawnBullet(_radius, 3);
                 break;
             }
         }
     }
 
-/// <summary>
-/// 
-/// </summary>
-/// <param name="radius"></param>
-/// <param name="num">たまの数</param>
-/// <param name="degree"></param>
+    /// <summary>
+    /// </summary>
+    /// <param name="radius"></param>
+    /// <param name="num">たまの数</param>
+    /// <param name="degree"></param>
     private void SpawnBullet(float radius = 100f, int num = 1, float degree = 0f)
     {
-        for (int i = 0; i < num; i++)
+        for (var i = 0; i < num; i++)
         {
             var bullet = _bulletPackedScene.Instantiate<RotateBook>();
             {
@@ -71,7 +68,7 @@ public partial class Book : WeaponBase
             _root.AddChild(bullet);
         }
     }
-    
+
 
     /// <summary>
     ///     複数の弾を同時に発射するときの初期位置のずれを設定するために利用する時間を計算する
@@ -80,12 +77,13 @@ public partial class Book : WeaponBase
     /// <param name="bulletNum">何番目の弾か</param>
     /// <param name="coolDown"></param>
     /// <returns></returns>
-    private float CalculateRelativePositon(int bulletTotalNum , int bulletNum, float coolDown)
+    private float CalculateRelativePositon(int bulletTotalNum, int bulletNum, float coolDown)
     {
         if (bulletNum == 0)
+        {
             return 0f;
-        
-        return (coolDown / bulletTotalNum) * bulletNum;
-    }
+        }
 
+        return coolDown / bulletTotalNum * bulletNum;
+    }
 }
