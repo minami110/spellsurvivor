e9f320de383a897a15896f8027ce9de40e6a5918
Minami Tomonobu <minami@edanoue.com>
2024-06-11 14:39:12 +0900

    Book をリファクタ

--- Infinity/Weapons/Book.cs
+++ Infinity/Weapons/Book.cs
@@ -1,4 +1,4 @@
-﻿using fms.Projectile;
+using fms.Projectile;
 using Godot;
 
 namespace fms.Weapon;
@@ -9,16 +9,6 @@ public partial class Book : WeaponBase
     [Export]
     private PackedScene _bulletPackedScene = null!;
 
-    [Export]
-    private Node _bulletSpawnNode = null!;
-
-    [Export]
-    // Pixel
-    private float _radius = 100f;
-
-    [Export]
-    private Node2D _root = null!;
-
     private protected override void DoAttack(uint level)
     {
         switch (level)
@@ -26,62 +16,44 @@ public partial class Book : WeaponBase
             // Level 1 は1つの弾をだす
             case 1:
             {
-                SpawnBullet(_radius);
+                SpawnBullet();
                 break;
             }
             // Level 2 は2つの弾をだす
             case 2:
             {
-                SpawnBullet(_radius, 2);
+                SpawnBullet(100, 2);
                 break;
             }
             // Level 3 以上は同じ
             default:
             {
-                SpawnBullet(_radius, 3);
+                SpawnBullet(100, 3);
                 break;
             }
         }
     }
 
-
-    /// <summary>
-    ///     複数の弾を同時に発射するときの初期位置のずれを設定するために利用する時間を計算する
-    /// </summary>
-    /// <param name="bulletTotalNum">同時に出す弾の数</param>
-    /// <param name="bulletNum">何番目の弾か</param>
-    /// <param name="coolDown"></param>
-    /// <returns></returns>
-    private float CalculateRelativePositon(int bulletTotalNum, int bulletNum, float coolDown)
-    {
-        if (bulletNum == 0)
-        {
-            return 0f;
-        }
-
-        return coolDown / bulletTotalNum * bulletNum;
-    }
-
     /// <summary>
     /// </summary>
     /// <param name="radius"></param>
-    /// <param name="num">たまの数</param>
-    /// <param name="degree"></param>
-    private void SpawnBullet(float radius = 100f, int num = 1, float degree = 0f)
+    /// <param name="spawnCount">たまの数</param>
+    private void SpawnBullet(float radius = 100f, int spawnCount = 1)
     {
-        for (var i = 0; i < num; i++)
+        for (var i = 0; i < spawnCount; i++)
         {
             var bullet = _bulletPackedScene.Instantiate<RotateBook>();
             {
-                bullet.BaseDamage = 50;
+                bullet.BaseDamage = 50f;
                 bullet.Radius = radius;
-                bullet.Angle = degree;
-                bullet.SecondPerRound = 3;
                 bullet.LifeFrame = SolvedCoolDownFrame;
-                bullet.InitTimeForRelativePos = CalculateRelativePositon(num, i, SolvedCoolDownFrame / 60f);
+                // 武器の持つクールダウンにあわせて360度回転するような速度を設定する
+                // Note: 360度 / クールダウン(フレーム) / 60 フレーム (= かかる秒数)
+                bullet.SpeedDegreePerSecond = 360f / (SolvedCoolDownFrame / 60f);
+                bullet.OffsetAngleDegree = 360f * i / spawnCount;
             }
-
-            _root.AddChild(bullet);
+            // Note: この Projectile はかってに GlobalPosition を更新するのでこっちで代入する必要はない
+            AddChild(bullet);
         }
     }
 }
