41ed438dbea5069dcbf60a4710af4d2256b85ae6
Minami Tomonobu <minami@edanoue.com>
2024-06-12 14:04:35 +0900

    Tween の R3 拡張を追加

---
+++ Common/Extensions/R3/TweenExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using Godot;
+
+namespace R3;
+
+public static class TweenExtensions
+{
+    public static Observable<Unit> FinishedAsObservable(this Tween tween,
+        CancellationToken cancellationToken = default)
+    {
+        return Observable.FromEvent(
+            h => tween.Finished += h,
+            h => tween.Finished -= h,
+            cancellationToken
+        );
+    }
+}
