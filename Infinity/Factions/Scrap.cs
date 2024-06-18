3c134be2a34a28f4a870067685bb79cdd222be47
kadomatsu <tktkcap.com@gmail.com>
2024-06-19 04:18:46 +0900

    :art: add comment and reformat

--- Infinity/Factions/Scrap.cs
+++ Infinity/Factions/Scrap.cs
@@ -1,19 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Godot;
 using R3;
-using System;
 
 namespace fms.Faction;
 
 /// <summary>
-///     Lv3: ショップフェイズ開始時、スクラップ を持つミニオンのレベルを 1 上げる
-///     Lv5: レベルが最大のウェポンの攻撃力を上げる
+/// Lv3: ショップフェイズ開始時、スクラップ を持つミニオンのレベルを 1 上げる
+/// Lv5: レベルが最大のウェポンの攻撃力を上げる
 /// </summary>
 [GlobalClass]
 public partial class Scrap : FactionBase
 {
+    public override bool IsActiveAnyEffect => Level >= 1;
+
     public override void _Ready()
     {
+        // はじめてこのシナジーのミニオンが購入されたときに、OnNextされてしまうので、Skip(1)している
         Main.WaveState.Phase.Skip(1).Subscribe(phase =>
         {
             if (phase != WavePhase.Shop)
@@ -22,11 +25,11 @@ public partial class Scrap : FactionBase
             }
 
             // 発動していない場合はリターンする
-            if (this.Level < 1)
+            if (Level < 3)
             {
                 return;
             }
-            
+
             var list = new List<Minion>();
             var nodes = this.GetSiblings();
             foreach (var node in nodes)
@@ -38,33 +41,30 @@ public partial class Scrap : FactionBase
 
                 if (minion.IsBelongTo(FactionType.Scrap))
                 {
-                    if(minion.IsMaxLevel)
+                    if (minion.IsMaxLevel)
                     {
                         continue;
                     }
-                    
+
                     list.Add(minion);
                 }
             }
-            
-            Random rand = new Random();
+
+            var rand = new Random();
 
             // レベルアップ対象がいない場合はリターン
             if (list.Count == 0)
             {
                 return;
             }
-            
+
             // スクラップを持つミニオンの中からランダムに選択
-            int randomIndex = rand.Next(0, list.Count); // 0 以上 numbers.Count 未満の乱数を生成
-            Minion target = list[randomIndex];
-            
+            var randomIndex = rand.Next(0, list.Count); // 0 以上 numbers.Count 未満の乱数を生成
+            var target = list[randomIndex];
+
             // レベルを上げる
             var currentLevel = target.Level.CurrentValue;
-            target.SetLevel(currentLevel + 1);            
-
+            target.SetLevel(currentLevel + 1);
         }).AddTo(this);
     }
-
-    public override bool IsActiveAnyEffect => Level >= 1;
 }
