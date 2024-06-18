1f620d6bb44bb1a14127156906ccb9d70e25f8e8
kadomatsu <tktkcap.com@gmail.com>
2024-06-19 06:56:09 +0900

    :bento: scrapについての表示を修正
    level 3 にならないと、発動中シナジーが白くならないように修正。また説明文も修正。

--- Infinity/Factions/Scrap.cs
+++ Infinity/Factions/Scrap.cs
@@ -7,12 +7,12 @@ namespace fms.Faction;
 
 /// <summary>
 /// Lv3: ショップフェイズ開始時、スクラップ を持つミニオンのレベルを 1 上げる
-/// Lv5: レベルが最大のウェポンの攻撃力を上げる
+/// Lv5: レベルが最大のウェポンの攻撃力を上げる (未実装)
 /// </summary>
 [GlobalClass]
 public partial class Scrap : FactionBase
 {
-    public override bool IsActiveAnyEffect => Level >= 1;
+    public override bool IsActiveAnyEffect => Level >= 3;
 
     public override void _Ready()
     {
