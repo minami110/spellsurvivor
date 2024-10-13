849081b2c05b1581ed235c2764fc35d66513696a
kadomatsu <tktkcap.com@gmail.com>
2024-10-14 02:42:13 +0900

    WIP

---
+++ Infinity/Weapons/GunTurret.cs
@@ -0,0 +1,36 @@
+﻿using fms.Projectile;
+using Godot;
+
+namespace fms.Weapon;
+/// <summary>
+///     GunTurretのクラス
+/// </summary>
+
+public partial class GunTurret : BaseProjectile
+{
+    // タレットが発射する弾
+    [Export]
+    private PackedScene _projectile = null!;
+    
+    private protected override void OnDamageEveryXFrames()
+    {
+        var bodies = GetOverlappingBodies();
+        if (bodies.Count == 0)
+            return;
+        var target = bodies[0];
+    }
+    // private protected override void SpawnProjectile(uint level)
+    // {
+    //     var aim = GetNode<AimToNearEnemy>("AimToNearEnemy");
+    //     var mazzle = GetNode<Node2D>("AimToNearEnemy/SpriteRoot/MazzlePoint");
+    //
+    //     if (!aim.IsAiming)
+    //     {
+    //         return;
+    //     }
+    //
+    //     var prj = _projectile.Instantiate<BaseProjectile>();
+    //     var dir = mazzle.GlobalTransform.X;
+    //     AddProjectile(prj, mazzle.GlobalPosition, dir);
+    // }
+}
---
+++ Infinity/Weapons/GunTurretGen.cs
@@ -0,0 +1,24 @@
+﻿using fms.Projectile;
+using Godot;
+
+namespace fms.Weapon;
+
+/// <summary>
+///     ガンタレットを生成する
+/// </summary>
+public partial class GunTurretGen : WeaponBase
+{
+    [Export]
+    private PackedScene _turret = null!;
+    
+    private uint _baseCoolDownFrame = 10u;
+
+    // Projectileじゃないけど、WeaponBaseの機構にタダ乗りするためにSpawnProjectileメソッドを利用
+    private protected override void SpawnProjectile(uint level)
+    {
+        // GunTurretを生成する。
+        // 今、味方のNPCを呼び出す共通機構がないためとりあえず個別実装で対応
+        var turret = _turret.Instantiate<GunTurret>();
+        //turret.AddChild();
+    }
+}
