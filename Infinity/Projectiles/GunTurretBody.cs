2e2987f0f7de64ad3481a23a9ebee84aacf4260d
Minami Tomonobu <minami@edanoue.com>
2024-11-01 18:22:29 +0900

    スクリプトを更新, タレットとショットガンの弾を共通化

--- Infinity/Projectiles/GunTurretBody.cs
+++ Infinity/Projectiles/GunTurretBody.cs
@@ -7,7 +7,7 @@ namespace fms.Weapon;
 ///     Weaponからこのクラスを生成し、このクラスから弾を生成する
 /// </summary>
 
-public partial class GunTurretBody : BaseProjectile
+public partial class GunTurretBody : AreaProjectile
 { 
     // タレットが発射する弾
     [Export]
@@ -16,17 +16,24 @@ public partial class GunTurretBody : BaseProjectile
     /// <summary>
     /// Projectile のダメージ処理をオーバーライドして新しい弾を生成する
     /// </summary>
-    private protected override void OnDamageEveryXFrames()
+    private protected override void Attack()
     {
         var bodies = GetOverlappingBodies();
         if (bodies.Count == 0)
             return;
+
+        // ToDo: 一番近い敵を選択する 
         var target = bodies[0];
 
         if (target is Enemy enemy)
         {
             // タレットが発射する弾を生成
             var prj = _projectileScene.Instantiate<BaseProjectile>();
+
+            // 弾のパラメーターを設定する
+            prj.Damage = 12;
+            prj.LifeFrame = 15;
+            prj.Speed = 800;
 
             // 一番近い敵に向かっていく
             prj.AddChild(new AutoAim
--- Infinity/Weapons/GunTurretGen.cs
+++ Infinity/Weapons/GunTurretGen.cs
@@ -9,16 +9,14 @@ namespace fms.Weapon;
 public partial class GunTurretGen : WeaponBase
 {
     [Export]
-    private PackedScene _turret = null!;
-    
-    private uint _baseCoolDownFrame = 10u;
+    private PackedScene _projectile = null!;
 
     // Projectileじゃないけど、WeaponBaseの機構にタダ乗りするためにSpawnProjectileメソッドを利用
     private protected override void SpawnProjectile(uint level)
     {
         // GunTurretを生成する。
         // 暫定的にturretはprojectileとして実装している  
-        var prj = _turret.Instantiate<BaseProjectile>();
+        var prj = _projectile.Instantiate<BaseProjectile>();
        
         // プレイヤーの位置にスポーンさせる
         AddProjectile(prj, GlobalPosition);
