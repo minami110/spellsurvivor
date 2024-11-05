df4a1310173d1d559e65b5957370102230ac1b7b
Minami Tomonobu <minami@edanoue.com>
2024-11-05 23:07:40 +0900

    レイヤーの Constant を追加

--- Infinity/Core/Constant.cs
+++ Infinity/Core/Constant.cs
@@ -4,6 +4,11 @@ namespace fms;
 
 public static class Constant
 {
+    public const int LAYER_PLAYER = 1 << 0;
+    public const int LAYER_DAMAGE = 1 << 1;
+    public const int LAYER_MOB = 1 << 2;
+    public const int LAYER_WALL = 1 << 4;
+
     public const uint MINION_MIN_LEVEL = 1u;
     public const uint MINION_MAX_LEVEL = 5u;
     public const uint MINION_MAX_TIER = 5u;
@@ -11,9 +16,10 @@ public static class Constant
     public const uint SHOP_MAX_LEVEL = 8u;
     public const uint SHOP_MAX_ITEM_SLOT = 8u;
 
-    public const uint PLYAER_INVENTORY_MAX_SLOT = 8u;
+    public const uint PLAYER_INVENTORY_MAX_SLOT = 8u;
 
-    public const uint FACTION_MAX_LEVEL = PLYAER_INVENTORY_MAX_SLOT;
+    public const uint FACTION_MAX_LEVEL = PLAYER_INVENTORY_MAX_SLOT;
+
 
     public static readonly StringName GroupNamePlayer = new("Player");
     public static readonly StringName GroupNamePlayerState = new("PlayerState");
@@ -23,4 +29,5 @@ public static class Constant
     public static readonly StringName GroupNameProjectile = new("Projectile");
     public static readonly StringName GroupNamePickableItem = new("PickableItem");
     public static readonly StringName GroupNameEnemy = new("Enemy");
+
 }
