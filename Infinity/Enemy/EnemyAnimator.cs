cb421662c4646c2035e913397ca640763fb29d9e
Minami Tomonobu <minami@edanoue.com>
2024-11-07 20:55:04 +0900

    敵のぷにぷにアニメーションを追加

---
+++ Infinity/Enemy/EnemyAnimator.cs
@@ -0,0 +1,84 @@
+using System;
+using Godot;
+
+namespace fms.Enemy;
+
+/// <summary>
+/// </summary>
+public partial class EnemyAnimator : Node
+{
+    /// <summary>
+    /// ぷにぷにする速度の Scale 
+    /// </summary>
+    [Export(PropertyHint.Range, "0.0,5.0")]
+    private float _puniSpeed = 1.0f;
+
+    /// <summary>
+    /// ぷにぷにする深さの Scale, もとの Sprite の y 方向の Scale に加算される
+    /// </summary>
+    [Export(PropertyHint.Range, "0.0,2.0")]
+    private float _puniDepth = 0.01f;
+
+    [Export]
+    private bool _invertFlipDirection = false;
+
+    private Vector2 _defaultScale = Vector2.One;
+    private EnemyBase _enemy = null!;
+    private Sprite2D _sprite = null!;
+
+    private float _puniLocation = 0.0f;
+
+    public override void _Ready()
+    {
+        var enemy = GetParentOrNull<EnemyBase>();
+        if (enemy is null)
+        {
+            throw new InvalidProgramException("親に EnemyBase が見つかりませんでした");
+        }
+
+        _enemy = enemy;
+
+        var sprite = GetNodeOrNull<Sprite2D>("../Sprite");
+
+        if (sprite is null)
+        {
+            throw new InvalidProgramException("兄弟に Sprite が見つかりませんでした");
+        }
+
+        _sprite = sprite;
+        _defaultScale = _sprite.Scale;
+    }
+
+    public override void _Process(double delta)
+    {
+        // Note: いろいろ仮です
+
+        // 現在の移動方向を取得する
+        var vel = _enemy.LinearVelocity;
+        var dir =  vel.Normalized();
+
+        // 右に移動している場合は右に, 左に移動している場合は左を向くように FlipH を制御する
+        if (dir.X > 0)
+        {
+            _sprite.FlipH = _invertFlipDirection;
+        }
+        else if (dir.X < 0)
+        {
+            _sprite.FlipH = !_invertFlipDirection;
+        }
+
+        // 移動に合わせて Sprite を上下にぷにぷに, 方向転換させる実装
+        // 物理演算による速度と, スポーン時に設定されたデータの速度のうち小さい方を取得する
+        // Note: ぶっ飛んでいるとき早くなりすぎず, 停止しているときはぷにぷにしないようにするため
+        var physicsSpeed = vel.Length();
+        var dataSpeed = _enemy.State.MoveSpeed.CurrentValue;
+        var speed = MathF.Min(physicsSpeed, dataSpeed);
+
+        // ぷにぷにの位置を更新する
+        _puniLocation += speed * _puniSpeed * (float)delta * 0.1f;
+
+        // Sprite を変形させる
+        var scaleY = MathF.Sin(_puniLocation) * _puniDepth;
+        _sprite.Scale = _defaultScale + new Vector2(0, scaleY);
+    }
+}
