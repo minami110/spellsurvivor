ce0249e7eb3cf525f3b3ef9bbcfb7f5c8b77bccb
Minami Tomonobu <minami@edanoue.com>
2024-11-12 15:40:49 +0900

    SniperRifle の素材をついあk

--- Infinity/Weapons/SniperRifle.cs
+++ Infinity/Weapons/SniperRifle.cs
@@ -9,6 +9,23 @@ public partial class SniperRifle : WeaponBase
     [Export]
     private PackedScene _projectile = null!;
 
+    [Export(PropertyHint.Range, "0,9999,1,suffix:px/s")]
+    private float _speed = 2000f;
+
+    [Export(PropertyHint.Range, "0,7200,1,suffix:frames")]
+    private uint _life = 15u;
+
+    /// <summary>
+    /// 攻撃を実行する際の敵の検索範囲
+    /// </summary>
+    [Export(PropertyHint.Range, "0,9999,1,suffix:px")]
+    private float _maxRange = 500f;
+
+    public override void _Ready()
+    {
+        GetNode<AimToNearEnemy>("AimToNearEnemy").SearchRadius = _maxRange;
+    }
+
     private protected override void SpawnProjectile(uint level)
     {
         var aim = GetNode<AimToNearEnemy>("AimToNearEnemy");
@@ -17,19 +34,20 @@ public partial class SniperRifle : WeaponBase
             return;
         }
 
-        var enemy = aim.FarthestEnemy;
+        var enemy = aim.FarthestEnemy!;
         var prj = _projectile.Instantiate<BulletProjectile>();
+        {
+            prj.Damage = BaseDamage;
+            prj.Knockback = Knockback;
+            prj.LifeFrame = _life;
+            prj.ConstantForce = (enemy.GlobalPosition - GlobalPosition).Normalized() * _speed;
+            prj.PenetrateEnemy = true;
+            prj.PenetrateWall = false;
+        }
 
-        prj.Damage = 12;
-        prj.LifeFrame = 15;
-        prj.Speed = 2000;
-        prj.PenetrateEnemy = true;
-        prj.PenetrateWall = false;
+        AddProjectile(prj, GlobalPosition);
 
-        AddProjectile(prj, GlobalPosition, enemy!.GlobalPosition - GlobalPosition);
-
-        // Note: AddTo はシーンに入れたあとしかできないので
-        // Projectile が Enemy にヒットしたら Stack を一つ貯める
+        // ToDo: 暫定実装, 敵にヒットするたびにダメージを半分にする
         prj.Hit
             .Where(x => x.HitNode is EnemyBase)
             .Subscribe(prj, (x, s) => { s.Damage *= 0.5f; })
