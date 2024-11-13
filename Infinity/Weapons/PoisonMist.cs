116cfb5e59e9315674b2ee4d6151026042aa0110
Minami Tomonobu <minami@edanoue.com>
2024-11-13 12:05:52 +0900

    PoisonMist を最新に

--- Infinity/Weapons/PoisonMist.cs
+++ Infinity/Weapons/PoisonMist.cs
@@ -8,21 +8,40 @@ namespace fms.Weapon;
 /// </summary>
 public partial class PoisonMist : WeaponBase
 {
-    [Export]
-    private PackedScene _projectile = null!;
+    /// <summary>
+    /// 毒沼の生存フレーム
+    /// </summary>
+    [Export(PropertyHint.Range, "1,1000,1,suffix:frames")]
+    private uint _aliveFrame = 300u;
+
+    /// <summary>
+    /// 毒沼の攻撃判定の間隔
+    /// </summary>
+    [Export(PropertyHint.Range, "1,1000,1,suffix:frames")]
+    private uint _attackSpan = 15u;
+
+    /// <summary>
+    /// 毒沼の大きさ
+    /// </summary>
+    [Export(PropertyHint.Range, "1,1000,1,suffix:px")]
+    private uint _size = 100u;
 
     private protected override void SpawnProjectile(uint level)
     {
-        var prj = _projectile.Instantiate<BaseProjectile>();
-        switch (level)
+        var prj = new CircleAreaProjectile();
+        prj.Damage = BaseDamage;
+        prj.Knockback = Knockback;
+        prj.LifeFrame = _aliveFrame;
+        prj.DamageEveryXFrames = _attackSpan;
+
+        // レベルに応じて毒沼のサイズを変更する
+        prj.Radius = level switch
         {
-            case 2:
-                prj.AddChild(new SizeMod { Add = 100 }); // 100 => 200
-                break;
-            case 3:
-                prj.AddChild(new SizeMod { Add = 200 }); // 100 => 300
-                break;
-        }
+            <= 1 => _size,
+            2 => _size + 20,
+            >= 3 => _size + 50
+        };
+        prj.Offset = Vector2.Zero;
 
         AddProjectile(prj, GlobalPosition);
     }
