f59483bd0a443338973be8a0266e52dc7692d696
Minami Tomonobu <minami@edanoue.com>
2024-11-13 12:41:59 +0900

    Parasite のシーンを改善

--- Infinity/Projectiles/ParasiteAreaDamage.cs
+++ Infinity/Projectiles/ParasiteAreaDamage.cs
@@ -3,74 +3,113 @@ using Godot;
 
 namespace fms.Weapon;
 
+/// <summary>
+/// 近くの敵に追従する AreaProjectile
+/// </summary>
 public partial class ParasiteAreaDamage : AreaProjectile
 {
     private State _state = State.SearchEnemy;
 
     private EnemyBase? _targetEnemy;
 
+    internal uint DamageRadius;
+
+    /// <summary>
+    /// 追従速度
+    /// </summary>
+    internal float FollowSpeed;
+
+    internal uint SearchRadius;
+
+    public override void _EnterTree()
+    {
+        // ダメージエリアのサイズを設定する
+        var col = GetNode<CollisionShape2D>("CollisionShape2D");
+        var circle = (CircleShape2D)col.Shape;
+        circle.Radius = DamageRadius;
+
+        // 敵検知範囲のサイズを設定する
+        var col2 = GetNode<CollisionShape2D>("EnemySearchField/CollisionShape2D");
+        var circle2 = (CircleShape2D)col2.Shape;
+        circle2.Radius = SearchRadius;
+    }
+
     public override void _PhysicsProcess(double delta)
     {
-        if (_state == State.SearchEnemy || _state == State.FollowPlayer)
+        if (_state is State.SearchEnemy or State.FollowParent)
         {
-            // 最も近い敵を検索する
-            var distance = float.MaxValue;
-            EnemyBase? nearest = null;
-            var bodies = GetNode<Area2D>("EnemySearchField").GetOverlappingBodies();
-            foreach (var body in bodies)
-            {
-                if (body is not EnemyBase enemy)
-                {
-                    continue;
-                }
-
-                var d = GlobalPosition.DistanceSquaredTo(enemy.GlobalPosition);
-                if (d < distance)
-                {
-                    distance = d;
-                    nearest = enemy;
-                }
-            }
-
-            // 最も近い追従する敵が見つかった
-            if (nearest is not null)
-            {
-                _state = State.FollowEnemy;
-                _targetEnemy = nearest;
-            }
-            // 敵が見つからなかったらプレイヤーに向かう
-            else
-            {
-                _state = State.FollowPlayer;
-            }
+            UpdateTargetAndState();
         }
 
-        if (_state == State.FollowPlayer)
+        switch (_state)
         {
-            // Weapon の位置に戻る
-            var pos = Weapon.GlobalPosition;
-            // 向きを変更する
-            Direction = (pos - GlobalPosition).Normalized();
-        }
-        // 敵追従
-        else if (_state == State.FollowEnemy)
-        {
-            // 敵がまた生きているかどうかを確認する
-            if (_targetEnemy!.IsDead)
+            // 親の位置に戻る
+            case State.FollowParent:
+            {
+                var pos = Weapon.GlobalPosition;
+                // 近ければ近いほどゆっくりにする速度バイアスを計算する
+                var bias = Mathf.Max(1f - GlobalPosition.DistanceTo(pos) / 50f, 0f);
+                ConstantForce = (pos - GlobalPosition).Normalized() * FollowSpeed * (1f - bias);
+                break;
+            }
+            // 敵が死んだら探索状態に戻る
+            case State.FollowEnemy when _targetEnemy!.IsDead:
             {
                 _state = State.SearchEnemy;
-                return;
+                break;
+            }
+            // 向きを変更する
+            case State.FollowEnemy:
+            {
+                var pos = _targetEnemy!.GlobalPosition;
+                // 近ければ近いほどゆっくりにする速度バイアスを計算する
+                var bias = Mathf.Max(1f - GlobalPosition.DistanceTo(pos) / 50f, 0f);
+                ConstantForce = (pos - GlobalPosition).Normalized() * FollowSpeed * (1f - bias);
+                break;
+            }
+        }
+    }
+
+    private void UpdateTargetAndState()
+    {
+        // 最も近い敵を検索する
+        var distance = float.MaxValue;
+        EnemyBase? nearest = null;
+        var bodies = GetNode<Area2D>("EnemySearchField").GetOverlappingBodies();
+        foreach (var body in bodies)
+        {
+            if (body is not EnemyBase enemy)
+            {
+                continue;
             }
 
-            // 向きを変更する
-            Direction = (_targetEnemy!.GlobalPosition - GlobalPosition).Normalized();
+            var d = GlobalPosition.DistanceSquaredTo(enemy.GlobalPosition);
+            if (d >= distance)
+            {
+                continue;
+            }
+
+            distance = d;
+            nearest = enemy;
+        }
+
+        // 最も近い追従する敵が見つかった
+        if (nearest is not null)
+        {
+            _state = State.FollowEnemy;
+            _targetEnemy = nearest;
+        }
+        // 敵が見つからなかったらプレイヤーに向かう
+        else
+        {
+            _state = State.FollowParent;
         }
     }
 
     private enum State
     {
         SearchEnemy,
-        FollowPlayer,
+        FollowParent,
         FollowEnemy
     }
 }
--- Infinity/Weapons/Parasite.cs
+++ Infinity/Weapons/Parasite.cs
@@ -1,5 +1,4 @@
-﻿using fms.Projectile;
-using Godot;
+﻿using Godot;
 
 namespace fms.Weapon;
 
@@ -8,10 +7,50 @@ public partial class Parasite : WeaponBase
     [Export]
     private PackedScene _projectile = null!;
 
+    /// <summary>
+    /// Parasite の攻撃間隔
+    /// </summary>
+    [Export(PropertyHint.Range, "1,1000,1,suffix:frames")]
+    private uint _attackSpan = 15u;
+
+    /// <summary>
+    /// パラサイトの移動速度
+    /// </summary>
+    [Export(PropertyHint.Range, "1,1000,1,suffix:px/s")]
+    private float _speed = 120f;
+
+    /// <summary>
+    /// 移動速度の個体差の ± の幅
+    /// </summary>
+    [Export(PropertyHint.Range, "0,1000,1,suffix:px/s")]
+    private float _randomSpeed = 20f;
+
+    [Export]
+    private uint _searchRadius = 150u;
+
+    [Export]
+    private uint _damageRadius = 20u;
+
+    /// <summary>
+    /// ウェーブ開始時に呼ばれるコールバック
+    /// </summary>
     private protected override void OnStartAttack()
     {
-        // 開始時に寿命が長い Projectile を生成する
-        var prj = _projectile.Instantiate<BaseProjectile>();
-        AddProjectile(prj, GlobalPosition);
+        // レベルに応じて数が増える
+        for (var i = 0; i < Level; i++)
+        {
+            var prj = _projectile.Instantiate<ParasiteAreaDamage>();
+            prj.Damage = BaseDamage;
+            prj.Knockback = Knockback;
+            prj.DamageEveryXFrames = _attackSpan;
+
+            var speed = _speed + (float)GD.Randfn(0f, _randomSpeed);
+            speed = Mathf.Max(speed, 0f);
+            prj.FollowSpeed = _speed + (float)GD.Randfn(0f, speed); // 個体差をもたせると狙う敵がバラける
+            prj.SearchRadius = _searchRadius;
+            prj.DamageRadius = _damageRadius;
+
+            AddProjectile(prj, GlobalPosition);
+        }
     }
 }
