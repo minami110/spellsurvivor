d1191db0261499ec5667f5aed0b9c1ac82ab2538
LM7 <69284966+lm7-pixel@users.noreply.github.com>
2024-11-19 03:17:46 +0900

    ライトのテスト
    ライトのテスト+仮HUDの調整

--- Infinity/Core/Projectiles/BulletProjectile.cs
+++ Infinity/Core/Projectiles/BulletProjectile.cs
@@ -7,131 +7,131 @@ namespace fms.Projectile;
 
 public partial class BulletProjectile : BaseProjectile
 {
-    [Export]
-    public bool PenetrateEnemy { get; set; }
+	[Export]
+	public bool PenetrateEnemy { get; set; }
 
-    [Export]
-    public bool PenetrateWall { get; set; }
+	[Export]
+	public bool PenetrateWall { get; set; }
 
-    private readonly List<ExcludeInfo> _excludes = [];
+	private readonly List<ExcludeInfo> _excludes = [];
 
-    public override void _Notification(int what)
-    {
-        base._Notification(what);
+	public override void _Notification(int what)
+	{
+		base._Notification(what);
 
-        if (what == NotificationReady)
-        {
-            this.BodyEnteredAsObservable()
-                .Subscribe(this, (x, s) => s.OnBodyEntered(x))
-                .AddTo(this);
-        }
-        else if (what == NotificationPhysicsProcess)
-        {
-            UpdateExcludes();
-        }
-    }
+		if (what == NotificationReady)
+		{
+			this.BodyEnteredAsObservable()
+				.Subscribe(this, (x, s) => s.OnBodyEntered(x))
+				.AddTo(this);
+		}
+		else if (what == NotificationPhysicsProcess)
+		{
+			UpdateExcludes();
+		}
+	}
 
-    private protected virtual void OnBodyEntered(Node2D body)
-    {
-        if (Age < SLEEP_FRAME)
-        {
-            return;
-        }
+	private protected virtual void OnBodyEntered(Node2D body)
+	{
+		if (Age < SLEEP_FRAME)
+		{
+			return;
+		}
 
 
-        // 壁など静的なオブジェクトとの衝突時の処理
-        // デフォルトでは壁と衝突したら常に自身は消滅する
-        if (body is StaticBody2D staticBody)
-        {
-            if (!PenetrateWall)
-            {
-                Kill(WhyDead.CollidedWithWall);
-            }
-        }
+		// 壁など静的なオブジェクトとの衝突時の処理
+		// デフォルトでは壁と衝突したら常に自身は消滅する
+		if (body is StaticBody2D staticBody)
+		{
+			if (!PenetrateWall)
+			{
+				Kill(WhyDead.CollidedWithWall);
+			}
+		}
 
-        // 敵との衝突時の処理
-        else if (body is IEntity entity)
-        {
-            // 除外リストに含まれている場合は無視
-            if (_excludes.Any(x => x.Node == body))
-            {
-                return;
-            }
+		// 敵との衝突時の処理
+		else if (body is IEntity entity)
+		{
+			// 除外リストに含まれている場合は無視
+			if (_excludes.Any(x => x.Node == body))
+			{
+				return;
+			}
 
-            // ToDo: IEntity に雑にキャスト
-            entity.ApplayDamage(Damage, (IEntity)Weapon.OwnedEntity, Weapon);
-            SendHitInfo(body);
+			// ToDo: IEntity に雑にキャスト
+			entity.ApplayDamage(Damage, (IEntity)Weapon.OwnedEntity, Weapon);
+			SendHitInfo(body);
 
-            // ToDo: Knockback 処理, 型があいまい
-            // Note: 死んでても死亡時アニメーションがあるのでノックバックを与える
-            if (body is EnemyBase enemy)
-            {
-                if (Knockback > 0)
-                {
-                    var impulse = PrevLinearVelocity.Normalized() * Knockback;
-                    enemy.ApplyKnockback(impulse);
-                }
-            }
+			// ToDo: Knockback 処理, 型があいまい
+			// Note: 死んでても死亡時アニメーションがあるのでノックバックを与える
+			if (body is EnemyBase enemy)
+			{
+				if (Knockback > 0)
+				{
+					var impulse = PrevLinearVelocity.Normalized() * Knockback;
+					enemy.ApplyKnockback(impulse);
+				}
+			}
 
-            /*
-            if (_hitSound != null)
-            {
-                SoundManager.PlaySoundEffect(_hitSound);
-            }
-            */
+			/*
+			if (_hitSound != null)
+			{
+				SoundManager.PlaySoundEffect(_hitSound);
+			}
+			*/
 
-            // 敵を貫通しないなら
-            if (!PenetrateEnemy)
-            {
-                Kill(WhyDead.CollidedWithEnemy);
-                return;
-            }
+			// 敵を貫通しないなら
+			if (!PenetrateEnemy)
+			{
+				Kill(WhyDead.CollidedWithEnemy);
+				return;
+			}
 
-            // 敵を貫通する場合 多段ヒットを防止するために除外リストに追加 
-            _excludes.Add(new ExcludeInfo
-            {
-                Node = body,
-                CreatedAge = Age
-            });
-        }
-    }
+			// 敵を貫通する場合 多段ヒットを防止するために除外リストに追加 
+			_excludes.Add(new ExcludeInfo
+			{
+				Node = body,
+				CreatedAge = Age
+			});
+		}
+	}
 
-    private void SendHitInfo(Node2D node)
-    {
-        // 最新の HitInfo を更新 (ダメージ処理のあとにやること)
-        // Note: すべての当たり判定が Sphere という決め打ちで法線を計算しています
-        HitInfo = new ProjectileHitInfo
-        {
-            HitNode = node,
-            Position = GlobalPosition,
-            Normal = (GlobalPosition - node.GlobalPosition).Normalized(),
-            Velocity = PrevLinearVelocity
-        };
+	private void SendHitInfo(Node2D node)
+	{
+		// 最新の HitInfo を更新 (ダメージ処理のあとにやること)
+		// Note: すべての当たり判定が Sphere という決め打ちで法線を計算しています
+		HitInfo = new ProjectileHitInfo
+		{
+			HitNode = node,
+			Position = GlobalPosition,
+			Normal = (GlobalPosition - node.GlobalPosition).Normalized(),
+			Velocity = PrevLinearVelocity
+		};
 
-        // Hit を通知する
-        _hitSubject.OnNext(HitInfo);
-    }
+		// Hit を通知する
+		_hitSubject.OnNext(HitInfo);
+	}
 
-    private void UpdateExcludes()
-    {
-        // 5 Frame 後には除外リストから削除
-        // Note: 5 Frame は適当な値っす
-        // リストを逆順にして削除していく
-        for (var i = _excludes.Count - 1; i >= 0; i--)
-        {
-            var exclude = _excludes[i];
-            if (Age - exclude.CreatedAge <= 5u)
-            {
-                continue;
-            }
+	private void UpdateExcludes()
+	{
+		// 5 Frame 後には除外リストから削除
+		// Note: 5 Frame は適当な値っす
+		// リストを逆順にして削除していく
+		for (var i = _excludes.Count - 1; i >= 0; i--)
+		{
+			var exclude = _excludes[i];
+			if (Age - exclude.CreatedAge <= 5u)
+			{
+				continue;
+			}
 
-            _excludes.RemoveAt(i);
-        }
-    }
+			_excludes.RemoveAt(i);
+		}
+	}
 
-    private struct ExcludeInfo
-    {
-        public Node2D Node { get; init; }
-        public uint CreatedAge { get; init; }
-    }
+	private struct ExcludeInfo
+	{
+		public Node2D Node { get; init; }
+		public uint CreatedAge { get; init; }
+	}
 }
--- Infinity/Player/BasePlayerPawn.cs
+++ Infinity/Player/BasePlayerPawn.cs
@@ -5,200 +5,200 @@ namespace fms;
 
 public enum PawnFaceDirection
 {
-    Right,
-    Left
+	Right,
+	Left
 }
 
 public partial class BasePlayerPawn : CharacterBody2D, IPawn, IEntity
 {
-    [Export(PropertyHint.Range, "0,1000,1")]
-    private float _health = 100f;
+	[Export(PropertyHint.Range, "0,1000,1")]
+	private float _health = 100f;
 
-    [Export(PropertyHint.Range, "0,1000,1,suffix:px/s")]
-    private float _moveSpeed = 100f;
+	[Export(PropertyHint.Range, "0,1000,1,suffix:px/s")]
+	private float _moveSpeed = 100f;
 
-    [Export]
-    private Vector2I _cameraLimit = new(550, 550);
+	[Export]
+	private Vector2I _cameraLimit = new(550, 550);
 
-    private readonly ReactiveProperty<PawnFaceDirection> _faceDirection = new(PawnFaceDirection.Right);
-    public ReadOnlyReactiveProperty<PawnFaceDirection> FaceDirection => _faceDirection;
+	private readonly ReactiveProperty<PawnFaceDirection> _faceDirection = new(PawnFaceDirection.Right);
+	public ReadOnlyReactiveProperty<PawnFaceDirection> FaceDirection => _faceDirection;
 
-    /// <summary>
-    /// </summary>
-    private Vector2 NextMoveDirection { get; set; }
+	/// <summary>
+	/// </summary>
+	private Vector2 NextMoveDirection { get; set; }
 
-    /// <summary>
-    /// 前フレームの移動方向
-    /// </summary>
-    private Vector2 PrevLinearVelocity { get; set; } = Vector2.Right;
+	/// <summary>
+	/// 前フレームの移動方向
+	/// </summary>
+	private Vector2 PrevLinearVelocity { get; set; } = Vector2.Right;
 
-    public override void _EnterTree()
-    {
-        AddToGroup(Constant.GroupNamePlayer);
-    }
+	public override void _EnterTree()
+	{
+		AddToGroup(Constant.GroupNamePlayer);
+	}
 
-    public override void _Ready()
-    {
-        // Inititialize PlayerState
-        var playerState = GetNode<PlayerState>("%PlayerState");
-        playerState.AddEffect(new AddMoveSpeedEffect { Value = _moveSpeed });
-        playerState.AddEffect(new AddHealthEffect { Value = _health });
-        playerState.AddEffect(new AddMaxHealthEffect { Value = _health });
+	public override void _Ready()
+	{
+		// Inititialize PlayerState
+		var playerState = GetNode<PlayerState>("%PlayerState");
+		playerState.AddEffect(new AddMoveSpeedEffect { Value = _moveSpeed });
+		playerState.AddEffect(new AddHealthEffect { Value = _health });
+		playerState.AddEffect(new AddMaxHealthEffect { Value = _health });
 
-        // Initialize Camera
-        var camera = GetNode<Camera2D>("%MainCamera");
-        camera.LimitLeft = -_cameraLimit.X;
-        camera.LimitRight = _cameraLimit.X;
-        camera.LimitTop = -_cameraLimit.Y;
-        camera.LimitBottom = _cameraLimit.Y;
+		// Initialize Camera
+		var camera = GetNode<Camera2D>("%MainCamera");
+		camera.LimitLeft = -_cameraLimit.X;
+		camera.LimitRight = _cameraLimit.X;
+		camera.LimitTop = -_cameraLimit.Y;
+		camera.LimitBottom = _cameraLimit.Y;
 
-        // Subscribes
-        playerState.MoveSpeed
-            .Subscribe(this, (x, self) => { self._moveSpeed = x; })
-            .AddTo(this);
-        _faceDirection.AddTo(this);
+		// Subscribes
+		playerState.MoveSpeed
+			.Subscribe(this, (x, self) => { self._moveSpeed = x; })
+			.AddTo(this);
+		_faceDirection.AddTo(this);
 
-        var healthBar = GetNodeOrNull<Range>("HealthBar");
-        if (healthBar is not null)
-        {
-            playerState.MaxHealth
-                .Subscribe(healthBar, (x, s) =>
-                {
-                    s.MaxValue = x;
-                    if (s.Value < s.MaxValue)
-                    {
-                        s.Show();
-                    }
-                    else
-                    {
-                        s.Hide();
-                    }
-                })
-                .AddTo(healthBar);
-            playerState.Health
-                .Subscribe(healthBar, (x, s) =>
-                {
-                    s.SetValueNoSignal(x);
-                    if (s.Value < s.MaxValue)
-                    {
-                        s.Show();
-                    }
-                    else
-                    {
-                        s.Hide();
-                    }
-                })
-                .AddTo(healthBar);
-        }
-    }
+		var healthBar = GetNodeOrNull<Range>("HealthBar");
+		if (healthBar is not null)
+		{
+			playerState.MaxHealth
+				.Subscribe(healthBar, (x, s) =>
+				{
+					s.MaxValue = x;
+					if (s.Value < s.MaxValue)
+					{
+						s.Show();
+					}
+					else
+					{
+						s.Hide();
+					}
+				})
+				.AddTo(healthBar);
+			playerState.Health
+				.Subscribe(healthBar, (x, s) =>
+				{
+					s.SetValueNoSignal(x);
+					if (s.Value < s.MaxValue)
+					{
+						s.Show();
+					}
+					else
+					{
+						s.Hide();
+					}
+				})
+				.AddTo(healthBar);
+		}
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        var controller = GetNode<BasePlayerAnimator>("%Animator");
+	public override void _PhysicsProcess(double delta)
+	{
+		var controller = GetNode<BasePlayerAnimator>("%Animator");
 
-        if (NextMoveDirection.LengthSquared() <= 0f)
-        {
-            controller.SendSignalStop();
-            return;
-        }
+		if (NextMoveDirection.LengthSquared() <= 0f)
+		{
+			controller.SendSignalStop();
+			return;
+		}
 
-        PrevLinearVelocity = NextMoveDirection;
-        var motion = PrevLinearVelocity * (float)delta * _moveSpeed;
-        MoveAndCollide(motion);
+		PrevLinearVelocity = NextMoveDirection;
+		var motion = PrevLinearVelocity * (float)delta * _moveSpeed;
+		MoveAndCollide(motion);
 
-        // Update Animation
-        controller.SendSignelMove();
-        if (motion.X > 0)
-        {
-            _faceDirection.Value = PawnFaceDirection.Right;
-            controller.SendSignalMoveRight();
-        }
-        else if (motion.X < 0)
-        {
-            _faceDirection.Value = PawnFaceDirection.Left;
-            controller.SendSignalMoveLeft();
-        }
-    }
+		// Update Animation
+		controller.SendSignelMove();
+		if (motion.X > 0)
+		{
+			_faceDirection.Value = PawnFaceDirection.Right;
+			controller.SendSignalMoveRight();
+		}
+		else if (motion.X < 0)
+		{
+			_faceDirection.Value = PawnFaceDirection.Left;
+			controller.SendSignalMoveLeft();
+		}
+	}
 
-    public void Heal(uint amount)
-    {
-        AddEffect(new HealEffect { Value = amount });
-        var info = new DamageReport
-        {
-            Amount = -amount,
-            Victim = this,
-            Instigator = this, // 自分自身で回復ということに..
-            Causer = this,
-            Position = GlobalPosition,
-            IsDead = false
-        };
-        StaticsManager.CommitDamage(info);
-    }
+	public void Heal(uint amount)
+	{
+		AddEffect(new HealEffect { Value = amount });
+		var info = new DamageReport
+		{
+			Amount = -amount,
+			Victim = this,
+			Instigator = this, // 自分自身で回復ということに..
+			Causer = this,
+			Position = GlobalPosition,
+			IsDead = false
+		};
+		StaticsManager.CommitDamage(info);
+	}
 
-    private void AddEffect(EffectBase effect)
-    {
-        GetNode<PlayerState>("%PlayerState").AddEffect(effect);
-    }
+	private void AddEffect(EffectBase effect)
+	{
+		GetNode<PlayerState>("%PlayerState").AddEffect(effect);
+	}
 
-    void IEntity.ApplayDamage(float amount, IEntity instigator, Node causer)
-    {
-        AddEffect(new PhysicalDamageEffect { Value = amount });
-        var info = new DamageReport
-        {
-            Amount = amount,
-            Victim = this,
-            Instigator = instigator,
-            Causer = causer,
-            Position = GlobalPosition,
-            IsDead = false
-        };
-        StaticsManager.CommitDamage(info);
-    }
+	void IEntity.ApplayDamage(float amount, IEntity instigator, Node causer)
+	{
+		AddEffect(new PhysicalDamageEffect { Value = amount });
+		var info = new DamageReport
+		{
+			Amount = amount,
+			Victim = this,
+			Instigator = instigator,
+			Causer = causer,
+			Position = GlobalPosition,
+			IsDead = false
+		};
+		StaticsManager.CommitDamage(info);
+	}
 
-    void IPawn.PrimaryPressed()
-    {
-        // Do nothing
-    }
+	void IPawn.PrimaryPressed()
+	{
+		// Do nothing
+	}
 
-    void IPawn.PrimaryReleased()
-    {
-        // Do nothing
-    }
+	void IPawn.PrimaryReleased()
+	{
+		// Do nothing
+	}
 
-    Vector2 IPawn.GetAimDirection()
-    {
-        // 現在狙っている方向を返す
-        // コントローラーがつながっている場合は, 右スティックの入力方法を返す
-        if (Input.GetConnectedJoypads().Count > 0)
-        {
-            var deadZone = 0.2f;
-            // ToDo: InputAction 使用したほうがいいかも
-            var joyX = Input.GetJoyAxis(0, JoyAxis.RightX);
-            if (joyX < deadZone && joyX > -deadZone)
-            {
-                joyX = 0;
-            }
+	Vector2 IPawn.GetAimDirection()
+	{
+		// 現在狙っている方向を返す
+		// コントローラーがつながっている場合は, 右スティックの入力方法を返す
+		if (Input.GetConnectedJoypads().Count > 0)
+		{
+			var deadZone = 0.2f;
+			// ToDo: InputAction 使用したほうがいいかも
+			var joyX = Input.GetJoyAxis(0, JoyAxis.RightX);
+			if (joyX < deadZone && joyX > -deadZone)
+			{
+				joyX = 0;
+			}
 
-            var joyY = Input.GetJoyAxis(0, JoyAxis.RightY);
-            if (joyY < deadZone && joyY > -deadZone)
-            {
-                joyY = 0;
-            }
+			var joyY = Input.GetJoyAxis(0, JoyAxis.RightY);
+			if (joyY < deadZone && joyY > -deadZone)
+			{
+				joyY = 0;
+			}
 
-            var joy = (Vector2.Right * joyX + Vector2.Down * joyY).Normalized();
+			var joy = (Vector2.Right * joyX + Vector2.Down * joyY).Normalized();
 
-            if (joy.LengthSquared() > 0)
-            {
-                return joy;
-            }
-        }
+			if (joy.LengthSquared() > 0)
+			{
+				return joy;
+			}
+		}
 
-        // つながっていない場合, スティックを倒していない場合は, 最後に動いた方向を返す
-        return PrevLinearVelocity.Normalized();
-    }
+		// つながっていない場合, スティックを倒していない場合は, 最後に動いた方向を返す
+		return PrevLinearVelocity.Normalized();
+	}
 
-    void IPawn.MoveForward(in Vector2 dir)
-    {
-        NextMoveDirection = dir;
-    }
+	void IPawn.MoveForward(in Vector2 dir)
+	{
+		NextMoveDirection = dir;
+	}
 }
