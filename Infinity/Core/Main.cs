1804d1280e94e5b5b8bbed624ab6dec4bd85e6ff
Minami Tomonobu <minami@edanoue.com>
2024-11-19 12:12:17 +0900

    Reformat

--- Infinity/Core/Main.cs
+++ Infinity/Core/Main.cs
@@ -8,158 +8,158 @@ namespace fms;
 
 public partial class Main : Node
 {
-	[ExportCategory("Settings")]
-	[Export]
-	private InfinityGameSettings _gameSettings = null!;
+    [ExportCategory("Settings")]
+    [Export]
+    private InfinityGameSettings _gameSettings = null!;
 
-	private static Main? _instance;
+    private static Main? _instance;
 
-	private PlayerState _playerState = null!;
-	private ShopState _shopState = null!;
-	private WaveState _waveState = null!;
+    private PlayerState _playerState = null!;
+    private ShopState _shopState = null!;
+    private WaveState _waveState = null!;
 
-	public static ShopState Shop
-	{
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		get => Instance._shopState;
-	}
+    public static ShopState Shop
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => Instance._shopState;
+    }
 
-	/// <summary>
-	/// Get the current WaveState
-	/// </summary>
-	public static WaveState WaveState
-	{
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		get => Instance._waveState;
-	}
+    /// <summary>
+    /// Get the current WaveState
+    /// </summary>
+    public static WaveState WaveState
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => Instance._waveState;
+    }
 
-	/// <summary>
-	/// Get Main instance
-	/// </summary>
-	/// <exception cref="ApplicationException"></exception>
-	private static Main Instance
-	{
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		get => _instance ?? throw new ApplicationException("Main instance is null");
-	}
+    /// <summary>
+    /// Get Main instance
+    /// </summary>
+    /// <exception cref="ApplicationException"></exception>
+    private static Main Instance
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => _instance ?? throw new ApplicationException("Main instance is null");
+    }
 
-	public Main()
-	{
-		if (_instance is null)
-		{
-			_instance = this;
-		}
-		else
-		{
-			throw new AggregateException("Main instance already exists");
-		}
-	}
+    public Main()
+    {
+        if (_instance is null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            throw new AggregateException("Main instance already exists");
+        }
+    }
 
-	public override void _EnterTree()
-	{
-		_waveState = new WaveState { Config = _gameSettings.WaveConfig };
-		AddChild(_waveState);
-		_shopState = new ShopState(_gameSettings.ShopConfig);
-		AddChild(_shopState);
-	}
+    public override void _EnterTree()
+    {
+        _waveState = new WaveState { Config = _gameSettings.WaveConfig };
+        AddChild(_waveState);
+        _shopState = new ShopState(_gameSettings.ShopConfig);
+        AddChild(_shopState);
+    }
 
-	public override void _Ready()
-	{
-		// PlayerState をキャッシュ
-		_playerState = (PlayerState)GetTree().GetFirstNodeInGroup(Constant.GroupNamePlayerState);
+    public override void _Ready()
+    {
+        // PlayerState をキャッシュ
+        _playerState = (PlayerState)GetTree().GetFirstNodeInGroup(Constant.GroupNamePlayerState);
 
-		// Battle Wave の開始時
-		var d1 = _waveState.Phase.Where(x => x == WavePhase.Battle).Subscribe(this, (_, state) =>
-		{
-			// ToDo: ダメージの Effect の解決方法決まり次第こちらも変える (#34)
-			// Playerの体力を全回復する
-			state._playerState.AddEffect(new AddHealthEffect
-				{ Value = state._playerState.MaxHealth.CurrentValue - state._playerState.Health.CurrentValue });
+        // Battle Wave の開始時
+        var d1 = _waveState.Phase.Where(x => x == WavePhase.Battle).Subscribe(this, (_, state) =>
+        {
+            // ToDo: ダメージの Effect の解決方法決まり次第こちらも変える (#34)
+            // Playerの体力を全回復する
+            state._playerState.AddEffect(new AddHealthEffect
+                { Value = state._playerState.MaxHealth.CurrentValue - state._playerState.Health.CurrentValue });
 
-			// Spawner に設定を渡す
-			var spawner = (EnemySpawnerBase)GetTree().GetFirstNodeInGroup("EnemySpawner");
-			spawner.SetConfig(state._waveState.CurrentWaveConfig.EnemySpawnConfig);
+            // Spawner に設定を渡す
+            var spawner = (EnemySpawnerBase)GetTree().GetFirstNodeInGroup("EnemySpawner");
+            spawner.SetConfig(state._waveState.CurrentWaveConfig.EnemySpawnConfig);
 
-			// すべての武器を起動する
-			var player = this.GetPlayerNode();
-			foreach (var n in player.GetChildren())
-			{
-				if (n is WeaponBase weapon)
-				{
-					weapon.StartAttack();
-				}
-			}
+            // すべての武器を起動する
+            var player = this.GetPlayerNode();
+            foreach (var n in player.GetChildren())
+            {
+                if (n is WeaponBase weapon)
+                {
+                    weapon.StartAttack();
+                }
+            }
 
-			// BGM のこもりを解消する (150hz => 2000hz (default))
-			var tween = CreateTween();
-			tween.TweenMethod(Callable.From((float value) => SoundManager.SetBgmBusLowPassFilterCutOff(value)), 150f,
-				2000f, 2d);
-		});
+            // BGM のこもりを解消する (150hz => 2000hz (default))
+            var tween = CreateTween();
+            tween.TweenMethod(Callable.From((float value) => SoundManager.SetBgmBusLowPassFilterCutOff(value)), 150f,
+                2000f, 2d);
+        });
 
-		// Battle Result 進入時
-		var d2 = _waveState.Phase.Where(x => x == WavePhase.Battleresult).Subscribe(this, (_, state) =>
-		{
-			var tree = state.GetTree();
+        // Battle Result 進入時
+        var d2 = _waveState.Phase.Where(x => x == WavePhase.Battleresult).Subscribe(this, (_, state) =>
+        {
+            var tree = state.GetTree();
 
-			// すべての武器を停止する
-			var player = this.GetPlayerNode();
-			foreach (var n in player.GetChildren())
-			{
-				if (n is WeaponBase weapon)
-				{
-					weapon.StopAttack();
-				}
-			}
+            // すべての武器を停止する
+            var player = this.GetPlayerNode();
+            foreach (var n in player.GetChildren())
+            {
+                if (n is WeaponBase weapon)
+                {
+                    weapon.StopAttack();
+                }
+            }
 
-			// 残った Projectile をすべてコロス
-			tree.CallGroup(Constant.GroupNameProjectile, Node.MethodName.QueueFree);
+            // 残った Projectile をすべてコロス
+            tree.CallGroup(Constant.GroupNameProjectile, Node.MethodName.QueueFree);
 
-			// 残った Item をすべてコロス
-			tree.CallGroup(Constant.GroupNamePickableItem, Node.MethodName.QueueFree);
-		});
+            // 残った Item をすべてコロス
+            tree.CallGroup(Constant.GroupNamePickableItem, Node.MethodName.QueueFree);
+        });
 
-		// Shop 進入時
-		var d3 = _waveState.Phase.Where(x => x == WavePhase.Shop).Subscribe(this, (_, state) =>
-		{
-			if (state._waveState.Wave.CurrentValue >= 0)
-			{
-				// Playerに報酬を与える
-				var reward = state._waveState.CurrentWaveConfig.Reward;
-				state._playerState.AddEffect(new MoneyEffect { Value = reward });
-			}
-			else
-			{
-				// PlayerController の初期化
-				ResetPlayerState();
-			}
+        // Shop 進入時
+        var d3 = _waveState.Phase.Where(x => x == WavePhase.Shop).Subscribe(this, (_, state) =>
+        {
+            if (state._waveState.Wave.CurrentValue >= 0)
+            {
+                // Playerに報酬を与える
+                var reward = state._waveState.CurrentWaveConfig.Reward;
+                state._playerState.AddEffect(new MoneyEffect { Value = reward });
+            }
+            else
+            {
+                // PlayerController の初期化
+                ResetPlayerState();
+            }
 
-			// Shop のリロール
-			state._shopState.RefreshInStoreMinions();
+            // Shop のリロール
+            state._shopState.RefreshInStoreMinions();
 
-			// BGM をこもらせる (2000hz(Default) => 150hz)
-			var tween = CreateTween();
-			tween.TweenMethod(Callable.From((float value) => SoundManager.SetBgmBusLowPassFilterCutOff(value)), 2000f,
-				150f, 0.5d);
-		});
+            // BGM をこもらせる (2000hz(Default) => 150hz)
+            var tween = CreateTween();
+            tween.TweenMethod(Callable.From((float value) => SoundManager.SetBgmBusLowPassFilterCutOff(value)), 2000f,
+                150f, 0.5d);
+        });
 
-		// Disposable registration
-		Disposable.Combine(d1, d2, d3).AddTo(this);
+        // Disposable registration
+        Disposable.Combine(d1, d2, d3).AddTo(this);
 
-		// Start Game
-		_waveState.SendSignal(WaveState.Signal.GamemodeStart);
-	}
+        // Start Game
+        _waveState.SendSignal(WaveState.Signal.GamemodeStart);
+    }
 
-	public override void _ExitTree()
-	{
-		if (_instance == this)
-		{
-			_instance = null;
-		}
-	}
+    public override void _ExitTree()
+    {
+        if (_instance == this)
+        {
+            _instance = null;
+        }
+    }
 
-	private void ResetPlayerState()
-	{
-		// Plauer を初期化する
-		_playerState.AddEffect(new MoneyEffect { Value = (int)_gameSettings.StartMoney });
-	}
+    private void ResetPlayerState()
+    {
+        // Plauer を初期化する
+        _playerState.AddEffect(new MoneyEffect { Value = (int)_gameSettings.StartMoney });
+    }
 }
--- Infinity/HUDs/InGameHUD.cs
+++ Infinity/HUDs/InGameHUD.cs
@@ -6,105 +6,105 @@ namespace fms;
 
 public sealed partial class InGameHUD : CanvasLayer
 {
-	[ExportSubgroup("Internal References")]
-	[Export]
-	private TextureProgressBar _healthBar = null!;
+    [ExportSubgroup("Internal References")]
+    [Export]
+    private TextureProgressBar _healthBar = null!;
 
-	[Export]
-	private Label _healthText = null!;
+    [Export]
+    private Label _healthText = null!;
 
-	[Export]
-	private Label _currentWaveLabel = null!;
+    [Export]
+    private Label _currentWaveLabel = null!;
 
-	[Export]
-	private Label _waveTimerLabel = null!;
+    [Export]
+    private Label _waveTimerLabel = null!;
 
-	[Export]
-	private Control _equipmentContainer = null!;
+    [Export]
+    private Control _equipmentContainer = null!;
 
-	[Export]
-	private PackedScene _equipmentPackedScene = null!;
+    [Export]
+    private PackedScene _equipmentPackedScene = null!;
 
-	// ToDo: 現在 Main 側の Wave 実装が適当なためこっちで管理する
-	private uint _currentWave;
+    // ToDo: 現在 Main 側の Wave 実装が適当なためこっちで管理する
+    private uint _currentWave;
 
-	public override void _Ready()
-	{
-		var playerState = (PlayerState)GetTree().GetFirstNodeInGroup(Constant.GroupNamePlayerState);
+    public override void _Ready()
+    {
+        var playerState = (PlayerState)GetTree().GetFirstNodeInGroup(Constant.GroupNamePlayerState);
 
-		// Subscribe player health
-		var d1 = playerState.Health.Subscribe(OnHealthChanged);
-		var d2 = playerState.MaxHealth.Subscribe(OnHealthChanged);
+        // Subscribe player health
+        var d1 = playerState.Health.Subscribe(OnHealthChanged);
+        var d2 = playerState.MaxHealth.Subscribe(OnHealthChanged);
 
-		// Subscribe wave info
-		var ws = Main.WaveState;
-		var d4 = ws.BattlePhaseTimeLeft.Subscribe(x => _waveTimerLabel.Text = $"{x:000}");
-		var d5 = ws.Phase.Subscribe(p =>
-		{
-			if (p == WavePhase.Battle)
-			{
-				_currentWave++;
-				_currentWaveLabel.Text = $"Wave {_currentWave}";
-				OnBattleWaveStarted();
-			}
-			else
-			{
-				OnBattleWaveEnded();
-			}
-		});
+        // Subscribe wave info
+        var ws = Main.WaveState;
+        var d4 = ws.BattlePhaseTimeLeft.Subscribe(x => _waveTimerLabel.Text = $"{x:000}");
+        var d5 = ws.Phase.Subscribe(p =>
+        {
+            if (p == WavePhase.Battle)
+            {
+                _currentWave++;
+                _currentWaveLabel.Text = $"Wave {_currentWave}";
+                OnBattleWaveStarted();
+            }
+            else
+            {
+                OnBattleWaveEnded();
+            }
+        });
 
-		// Add disposables when this node is exited tree
-		Disposable.Combine(d1, d2, d4, d5).AddTo(this);
-	}
+        // Add disposables when this node is exited tree
+        Disposable.Combine(d1, d2, d4, d5).AddTo(this);
+    }
 
-	private void OnBattleWaveEnded()
-	{
-		// Hide 
-		Hide();
+    private void OnBattleWaveEnded()
+    {
+        // Hide 
+        Hide();
 
-		if (_equipmentContainer.GetChildCount() == 0)
-		{
-			return;
-		}
+        if (_equipmentContainer.GetChildCount() == 0)
+        {
+            return;
+        }
 
-		foreach (var child in _equipmentContainer.GetChildren())
-		{
-			child.QueueFree();
-		}
-	}
+        foreach (var child in _equipmentContainer.GetChildren())
+        {
+            child.QueueFree();
+        }
+    }
 
-	private void OnBattleWaveStarted()
-	{
-		// Spawn equipments
-		var nodes = GetTree().GetNodesInGroup(Constant.GroupNameWeapon);
-		foreach (var n in nodes)
-		{
-			if (n is not WeaponBase weapon)
-			{
-				continue;
-			}
+    private void OnBattleWaveStarted()
+    {
+        // Spawn equipments
+        var nodes = GetTree().GetNodesInGroup(Constant.GroupNameWeapon);
+        foreach (var n in nodes)
+        {
+            if (n is not WeaponBase weapon)
+            {
+                continue;
+            }
 
-			var node = _equipmentPackedScene.Instantiate<InGameEquipment>();
-			{
-				node.Weapon = weapon;
-			}
-			_equipmentContainer.AddChild(node);
-		}
+            var node = _equipmentPackedScene.Instantiate<InGameEquipment>();
+            {
+                node.Weapon = weapon;
+            }
+            _equipmentContainer.AddChild(node);
+        }
 
-		// Show
-		Show();
-	}
+        // Show
+        Show();
+    }
 
-	private void OnHealthChanged(float _)
-	{
-		var playerState = (PlayerState)GetTree().GetFirstNodeInGroup(Constant.GroupNamePlayerState);
+    private void OnHealthChanged(float _)
+    {
+        var playerState = (PlayerState)GetTree().GetFirstNodeInGroup(Constant.GroupNamePlayerState);
 
-		// Update health bar
-		_healthBar.MinValue = 0f;
-		_healthBar.MaxValue = playerState.MaxHealth.CurrentValue;
-		_healthBar.Value = playerState.Health.CurrentValue;
+        // Update health bar
+        _healthBar.MinValue = 0f;
+        _healthBar.MaxValue = playerState.MaxHealth.CurrentValue;
+        _healthBar.Value = playerState.Health.CurrentValue;
 
-		// Update health text
-		_healthText.Text = $"{playerState.Health.CurrentValue} / {playerState.MaxHealth.CurrentValue}";
-	}
+        // Update health text
+        _healthText.Text = $"{playerState.Health.CurrentValue} / {playerState.MaxHealth.CurrentValue}";
+    }
 }
