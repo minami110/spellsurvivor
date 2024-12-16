d52cf9deab65d944c25b708039ea9b5de72bd588
Minami Tomonobu <minami@edanoue.com>
2024-12-16 13:10:10 +0900

    Impl walking animation

--- Infinity/Player/SkinAnimator.cs
+++ Infinity/Player/SkinAnimator.cs
@@ -6,6 +6,8 @@ public partial class SkinAnimator : BasePlayerAnimator
 {
     private static readonly StringName TrackNameIdle = new("idle");
     private static readonly StringName TrackNameRunning = new("running");
+    private static readonly StringName TrackNameWalking = new("walking");
+
     private Vector2 _defaultScale;
     private AnimationPlayer _player = null!;
     private Node2D _skin = null!;
@@ -19,9 +21,17 @@ public partial class SkinAnimator : BasePlayerAnimator
 
     private protected override void SendSignalMove(double delta)
     {
-        _player.Play(TrackNameRunning);
-
         var currentSpeed = LinearVelocity.Length() / (float)delta;
+
+        if (_player.HasAnimation(TrackNameWalking) && currentSpeed < 100f)
+        {
+            _player.Play(TrackNameWalking, 0.1d);
+        }
+        else
+        {
+            _player.Play(TrackNameRunning, 0.1d);
+        }
+
         _player.SpeedScale = currentSpeed / 100f; // アニメーションが 100px/s 想定
     }
 
